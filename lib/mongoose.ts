952973561e2127584b77797c4700d2e0a7ab87bb
Arkopravo Das <arko.andrew2000@gmail.com>
2024-04-04 05:21:01 +0530

    create question and display question on home page

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true); // prevents unknown field queries
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
+  }
+
+  if (isConnected) {
+    return console.log("MongoDB is already connected");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "DevFlux",
+    });
+    isConnected = true;
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+  }
+};
